655f718fcb6ba265bcae11311da214ab37d5c669
airenliao <airenliao@gmail.com>
2013-03-04 22:21:31 +0800

    add the html file

---
+++ block-weibo.php
@@ -0,0 +1,13 @@
+<div class="block block-weibo">
+	<h2 class="block-title">有什么新鲜事想告诉大家</h2>
+	<div class="block-content">
+		<div class="input">
+			<textarea name="" id="" cols="30" rows="10" class="form-text"></textarea>
+			<span class="icon ir icon-arrow">arrow</span>
+		</div>
+		<div class="func_area clearfix">
+			<div class="btn-wrap fr"><a href="#" class="btn btn-send">发布</a></div>
+			<div class="upload-img fr"><a href=""><i class="icon ir icon-upload-img"></i>照片</a></div>
+		</div>
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+/*
+*doctype file: html.php
+*header content: header.php
+*footer content: footer.php
++==============================+
+*page login: login.php
+*/
+?>
+
+<?php include("html.php")?>
+<body>
+	<div class="wrapper">
+		<?php include("header.php")?>
+		<!-- end header content -->
+		<div id="page">
+			<div class="container clearfix">
+				<div class="container-wrapper">
+					<div class="container-inner">
+						<div id="main-content" class="clearfix">
+							<div class="content fl">
+								<?php include("block-weibo.php")?>
+							</div>	 
+							<!-- end main contnet -->
+							<div class="sidebar fl" id="second-sidebar">
+								Right sidebar content 
+							</div>
+							<!-- end second sidebar content -->
+						</div>
+						<!-- end main content -->
+						<div id="first-sidebar" class="sidebar clearfix">
+							<?php include("user-profile.php")?>
+							<?php include("ranking.php")?>
+						</div>					
+						<!-- end first-sidebar content -->
+					</div>
+				</div>				
+			</div>
+		</div>
+		<!-- end page content -->
+		<?php include("footer.php")?>
+		<!-- end footer content -->
+	</div>	
+</body>
+</html>
