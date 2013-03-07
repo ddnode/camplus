4ed03e7b4595b2fd90d7011d44c132b037af1f2a
dansboy <dansboy@1907000.no-reply.drupal.org>
2013-03-08 00:00:50 +0800

    add header template

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -78,56 +78,10 @@
     <div class="container clearfix">
       <h1 id="logo" class="fl pts mrs"><a href="<?php print $front_page; ?>" class="ir icon icon-logo"><?php print $site_name; ?></a></h1>
       <!-- end logo -->
-      <ul id="nav" class="clearfix unstyled fl" rel="navigation">
-        <li><a href="#" title="校园操场" class="active">校园操场</a></li>
-        <li class="haschild">
-          <a href="#" title="校园社区">校园社区</a>
-          <ol class="sub-menu unstyled">
-            <li><a href="#">sub menu</a></li>
-            <li><a href="#">sub menu</a></li>
-            <li><a href="#">sub menu</a></li>
-            <li><a href="#">sub menu</a></li>
-            <li><a href="#">sub menu</a></li>
-          </ol>
-        </li>
-        <li class="haschild">
-          <a href="#" title="校园知识">校园知识</a>
-          <ol class="sub-menu unstyled">
-            <li><a href="#">sub menu</a></li>
-            <li><a href="#">sub menu</a></li>
-            <li><a href="#">sub menu</a></li>
-            <li><a href="#">sub menu</a></li>
-            <li><a href="#">sub menu</a></li>
-          </ol>
-        </li>
-        <li class="haschild">
-          <a href="#" title="校园博客">校园博客</a>
-          <ol class="sub-menu unstyled">
-            <li><a href="#">sub menu</a></li>
-            <li><a href="#">sub menu</a></li>
-            <li><a href="#">sub menu</a></li>
-            <li><a href="#">sub menu</a></li>
-            <li><a href="#">sub menu</a></li>
-          </ol>
-        </li>
-      </ul>
-      <!-- end nav menu -->
-
-      <form action="" method="post" class="search-form fl pr">
-        <input type="text" class="form-text" placeholder="找人" />
-        <button class="icon ir icon-search" type="submit">搜索</button>
-      </form>
-      <!-- end search form -->
-
-      <ul class="secondary-menu unstyled clearfix fl">
-        <li><a href="#"><i class="icon ir icon-account">用户</i></a></li>
-        <li><a href="#"><i class="icon ir icon-msg">信息</i></a><i class="msg-num">1</i></li>
-        <li><a href="#"><i class="icon ir icon-logout">退出</i></a></li>
-      </ul>
-      <!-- end secondary menu -->
+      <?php print render($page['header']); ?>
     </div>
   </div>
-  <?php print render($page['header']); ?>
+  
   <!-- end header content -->
   <?php print $messages; ?>
   <?php print render($page['help']); ?>
